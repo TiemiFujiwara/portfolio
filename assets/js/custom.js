59c3c8170d6405e339a80f280edf543946a36c85
TiemiFujiwara <mariaclarafujiwara@gmail.com>
2026-08-17 15:19:13 -0300

    [Adição do recurso de Reveal Scroll - Editado arquivo index.html nas linhas 40,41,80,96,104,134,169,181,184 e 211 e custom.css nas linhas 9,10,16,17,23,24,132,134,140,141,145,146,418 a 429,534 a 539 e 746 e custom.js 1 a 8 e27 a 43]

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,11 @@
+/* Custom JS */
+/* 
+1. Mobile Nav Bar - Hamburger Menu Feature
+2. Scroll Reveal Feature
+*/
+
+/* 1. Mobile Nav Bar - Hamburger Menu Feature */
+
 document.addEventListener('DOMContentLoaded', () => {
   const btnBurger = document.getElementById('btnhamburger');
   const navList = document.querySelector('.navlist');
@@ -15,4 +23,21 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     });
   }
+});
+
+/* 2. Scroll Reveal Feature */
+
+document.addEventListener('DOMContentLoaded', () => {
+  const viewer = new IntersectionObserver((opensite, viewer) => {
+    opensite.forEach(opensite => {
+      if (opensite.isIntersecting) {
+        opensite.target.classList.add('active');
+        viewer.unobserve(opensite.target);
+      }
+    });
+  });
+
+  const allelementsreveal = document.querySelectorAll('.reveal');
+  allelementsreveal.forEach(element => viewer.observe(element));
+
 });
