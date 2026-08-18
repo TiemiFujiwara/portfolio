503bd974abb15bf55127809dd0277b32e4f205de
TiemiFujiwara <mariaclarafujiwara@gmail.com>
2026-08-18 20:39:04 -0300

    [Adição da Feature Parallax Mouse na Hero Section - Editado index.html nas linhas 48 a 64 arquivo cusotm.css nas linhas 10,17,23,24,23,24,144,148,293,473 a 479,709 a 715,950 a 1049 e custom.js nas linhas 4,5,8,29,45 a 74]

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,11 @@
 /* Custom JS */
 /* 
 1. Mobile Nav Bar - Hamburger Menu Feature
-2. Scroll Reveal Feature
+2. Desktop - Scroll Reveal Feature
+3. Desktop - Parallax Mouse Feature
 */
 
-/* 1. Mobile Nav Bar - Hamburger Menu Feature */
+// 1. Mobile Nav Bar - Hamburger Menu Feature 
 
 document.addEventListener('DOMContentLoaded', () => {
   const btnBurger = document.getElementById('btnhamburger');
@@ -25,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
-/* 2. Scroll Reveal Feature */
+// 2. Desktop - Scroll Reveal Feature
 
 document.addEventListener('DOMContentLoaded', () => {
   const viewer = new IntersectionObserver((opensite, viewer) => {
@@ -41,3 +42,33 @@ document.addEventListener('DOMContentLoaded', () => {
   allelementsreveal.forEach(element => viewer.observe(element));
 
 });
+
+// 3. Desktop - Parallax Mouse Feature
+
+const headerDisplay = document.querySelector('.header-display');
+const front = document.querySelector('.frontlayer');
+const mid = document.querySelector('.midlayer');
+const back = document.querySelector('.backlayer');
+
+const sFront = 20;
+const sMid = 45;
+const sBack = 70;
+
+if (headerDisplay && front && mid && back) {
+  headerDisplay.addEventListener('mousemove', e => {
+    const rect = headerDisplay.getBoundingClientRect();
+
+    const x = e.clientX - (rect.left + rect.width / 2);
+    const y = e.clientY - (rect.top + rect.height / 2);
+
+    front.style.transform = `translate3d(${x / sFront}px, ${y / sFront}px, 0)`;
+    mid.style.transform = `translate3d(${x / sMid}px, ${y / sMid}px, 0)`;
+    back.style.transform = `translate3d(${x / sBack}px, ${y / sBack}px, 0)`;
+  });
+
+  headerDisplay.addEventListener('mouseleave', () => {
+    front.style.transform = `translate3d(0, 0, 0)`;
+    mid.style.transform = `translate3d(0, 0, 0)`;
+    back.style.transform = `translate3d(0, 0, 0)`;
+  });
+};
